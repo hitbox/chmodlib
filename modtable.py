3c3b67efffef3f87151943b186fc5c44b6671dac
Carl Harris <elgoogemail2007@gmail.com>
2022-03-10 23:15:24 -0500

    modtable.py: add comments and docs, cleaner code

--- modtable.py
+++ modtable.py
@@ -6,22 +6,23 @@ from oct2rwx import oct2rwx
 
 def main(argv=None):
     """
+    Print table of all possible rwx.
     """
     parser = argparse.ArgumentParser(prog=Path(__file__).stem,
             description=main.__doc__)
     args = parser.parse_args(argv)
 
+    # build list of liens - header
     lines = [('Dec', 'Bin', 'RWX')]
+    # all values for decimal, binary, and rwx.
     lines += [ (str(i), '{:>03}'.format(bin(i)[2:]), oct2rwx(i)) for i in range(8) ]
-
-    lengths = (tuple(len(v) for v in tup) for tup in lines)
-    columns = zip(*lengths)
-    maxes = tuple(max(column) for column in columns)
-
+    # max length of each column
+    maxes = [max(map(len, column)) for column in zip(*lines)]
+    # per-column formatters
+    formatters = list('{:>%s}' % width for width in maxes)
+    # format and print each line
     for tup in lines:
-        formatters = list('{:>%s}' % width for width in maxes)
-        line = ' '.join(f.format(s) for s, f in zip(tup, formatters))
-        print(line)
+        print(' '.join(f.format(s) for s, f in zip(tup, formatters)))
 
 if __name__ == '__main__':
     main()
